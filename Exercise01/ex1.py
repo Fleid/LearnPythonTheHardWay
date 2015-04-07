d296ad395d887210d391b3654c36e175f05220bc
Florian Eiden <florian.eiden@fleid.fr>
2015-04-07 16:08:53 +0200

    ex1.py

---
+++ Exercise01/ex1.py
@@ -0,0 +1,9 @@
+# cd ~/Documents/GitHub/LearnPythonTheHardWay/Exercise01/
+
+print "Hello World!"
+print "Hello again"
+print "I like typing this"
+print "This is fun"
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
