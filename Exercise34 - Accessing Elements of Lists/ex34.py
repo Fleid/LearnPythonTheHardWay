f55608530a4da691b480e4c7d8e781ecb5f851e2
Florian Eiden <florian.eiden@fleid.fr>
2015-04-13 16:24:07 +0200

    34

---
+++ Exercise34 - Accessing Elements of Lists/ex34.py
@@ -0,0 +1,15 @@
+# cd ~/Documents/GitHub/LearnPythonTheHardWay/Exercise34
+# -*- coding: utf-8 -*-
+
+animals = ['bear','python','peacock','kangaroo','whale','platypus']
+
+print animals
+
+print "Index 1 :", animals[1]
+print "Order 3 :", animals[2]
+print "Order 1 :", animals[0]
+print "Index 3 :", animals[3]
+print "Order 5 :", animals[4]
+print "Index 2 :", animals[2]
+print "Order 6 :", animals[6-1]
+print "Index 4 :", animals[4]
