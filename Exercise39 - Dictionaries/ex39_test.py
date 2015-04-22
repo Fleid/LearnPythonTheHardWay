1212299517d9af5d37abea1f580f85cfbfc71e89
Florian Eiden <florian.eiden@fleid.fr>
2015-04-22 17:34:19 +0200

    39 bis

--- Exercise39 - Dictionaries/ex39_test.py
+++ Exercise39 - Dictionaries/ex39_test.py
@@ -11,3 +11,8 @@ hashmap.set(states, 'California', 'CA')
 hashmap.set(states, 'New York', 'NY')
 hashmap.set(states, 'Michigan', 'MI')
 
+# crate a basic set of states and some cities in them
+cities = hashmap.new()
+hashmap.set(cities, 'CA', 'San Francisco')
+hashmap.set(cities, 'MI', 'Detroit')
+hashmap.set(cities, 'FL', 'Jacksonville')
