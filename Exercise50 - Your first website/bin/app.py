7cd95e745ed84223f2d31e4b8a6df614b13bbbc5
Florian Eiden <florian.eiden@fleid.fr>
2015-05-26 10:53:09 +0200

    50 final + web to db

--- Exercise50 - Your first website/bin/app.py
+++ Exercise50 - Your first website/bin/app.py
@@ -1,19 +1,25 @@
 # -*- coding: utf-8 -*-
 
+# Pour me lancer : python bin/app.py
+
 import web
 
+# URL Handling : première partie est le regex, ensuite la classe que ça appelle
 urls = (
 	'/', 'Index'
 )
 
+# Les objets du framework
 app = web.application(urls, globals())
-
 render = web.template.render('templates/')
 
+# Ma page Index, déclarée dans l'urls, avec ces méthodes http
 class Index(object):
 	def GET(self):
-		greeting = "Hello World!"
+		greeting = "Hello there!"
+		# La syntaxe magique : render.XXX, XXX le nom du fichier dans \templates
 		return render.index(greeting = greeting)
 
+# Code magique
 if __name__ == "__main__":
 	app.run()
